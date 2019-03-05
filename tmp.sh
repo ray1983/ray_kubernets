46adb91ceb175cfb81571be2b14a9b7c7913398d
ray1983 <dulei1983@126.com>
2019-03-05 10:50:36 +0800

    first commit

--- tmp.sh
+++
@@ -1,6 +0,0 @@
-echo "# ray_kubernets" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/ray1983/ray_kubernets.git
-git push -u origin master
